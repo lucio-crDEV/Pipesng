fe9d54b37a23da51fb10d8867f5854e8563718c4
lucio-crDEV <lucio.m.cardenas@gmail.com>
2022-06-01 20:47:38 -0400

    retomando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+
+// PrimeNg
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
